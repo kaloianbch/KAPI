69c060a7b72f995c3a2a4276ac5e7840c5799113
coo_coo1 <github@chokanov.com>
2020-10-14 00:54:40 +0100

    feat: add GPS script template with explanation

---
+++ KAPI/GPS_startup_template.lua
@@ -0,0 +1,6 @@
+-- change label name and position(remember y is -1 when standing on top of PC)
+-- the name of this file has to be "startup"
+-- guide for building the structure:
+-- (http://www.computercraft.info/forums2/index.php?/topic/3088-how-to-guide-gps-global-position-system/)
+shell.run("label","set","CHANGE")
+shell.run("gps", "host", x,y,z)
