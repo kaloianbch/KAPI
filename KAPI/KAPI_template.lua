468ae96165c1b0307b40e55f6a96692c8d673b94
coo_coo1 <github@chokanov.com>
2020-10-16 15:27:58 +0100

    chore: Change KAPI program template to be more accesible

--- KAPI/KAPI_template.lua
+++ KAPI/KAPI_template.lua
@@ -2,11 +2,6 @@ if not (os.loadAPI("KAPI")) then
     error("Could not load KAPI")
 end
 
-KAPI.setProgName("Template")
---KAPI.setStartingCard(0)
---KAPI.setMoveTimeout(10)
---KAPI.setStorageBlockID("minecraft:chest")
-
 --[Constansts]--
 
 --[Globals]--
@@ -14,11 +9,8 @@ KAPI.setProgName("Template")
 --[Functions]--
 
 --[Main]--
-
+KAPI.setProgName("Template")
+--You can and should set the other KAPI globals as needed
 KAPI.init()
-origin = KAPI.getOrigin()
-print("origin: ", origin:tostring())
-KAPI.goTo()
-pos = KAPI.updateLastPos()
-print("current: ", pos:tostring())
-KAPI.goTo(origin)
+--logic goes here
+KAPI.kill()
